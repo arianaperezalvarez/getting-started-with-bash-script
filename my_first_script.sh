550b650ade6bc75d064e9f6cbf8c477170a8ee13
arianaperezalvarez <149642250+arianaperezalvarez@users.noreply.github.com>
2023-11-23 19:29:38 +0000

    extra credit wooooh

--- my_first_script.sh
+++ my_first_script.sh
@@ -48,44 +48,4 @@ function all_checks() {
 	kernel_check
 }
 
-##
-# Color  Variables
-##
-green='\e[32m'
-blue='\e[34m'
-clear='\e[0m'
-
-##
-# Color Functions
-##
-
-ColorGreen(){
-	echo -ne $green$1$clear
-}
-ColorBlue(){
-	echo -ne $blue$1$clear
-}
-
-menu(){
-echo -ne "
-My First Menu
-$(ColorGreen '1)') Memory usage
-$(ColorGreen '2)') CPU load
-$(ColorGreen '3)') Number of TCP connections 
-$(ColorGreen '4)') Kernel version
-$(ColorGreen '5)') Check All
-$(ColorGreen '0)') Exit
-$(ColorBlue 'Choose an option:') "
-        read a
-        case $a in
-	        1) memory_check ; menu ;;
-	        2) cpu_check ; menu ;;
-	        3) tcp_check ; menu ;;
-	        4) kernel_check ; menu ;;
-	        5) all_checks ; menu ;;
-		0) exit 0 ;;
-		*) echo -e $red"Wrong option."$clear; WrongCommand;;
-        esac
-}
-
-# Call the menu function
+all_checks
